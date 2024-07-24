323040cb9714c37d1123d1896e297ba16c9c2b27
Nazim Gafarov <zapolnoch@gmail.com>
2024-07-24 03:48:29 +0300

    format: code

--- components/ui/glare-card.tsx
+++ components/ui/glare-card.tsx
@@ -1,15 +1,15 @@
-import { cn } from "@/lib/utils";
-import { useRef } from "react";
+import { cn } from "@/lib/utils"
+import { useRef } from "react"
 
 export const GlareCard = ({
   children,
   className,
 }: {
-  children: React.ReactNode;
-  className?: string;
+  children: React.ReactNode
+  className?: string
 }) => {
-  const isPointerInside = useRef(false);
-  const refElement = useRef<HTMLDivElement>(null);
+  const isPointerInside = useRef(false)
+  const refElement = useRef<HTMLDivElement>(null)
   const state = useRef({
     glare: {
       x: 50,
@@ -23,7 +23,7 @@ export const GlareCard = ({
       x: 0,
       y: 0,
     },
-  });
+  })
   const containerStyle = {
     "--m-x": "50%",
     "--m-y": "50%",
@@ -37,7 +37,7 @@ export const GlareCard = ({
     "--radius": "48px",
     "--easing": "ease",
     "--transition": "var(--duration) var(--easing)",
-  } as any;
+  } as any
 
   const backgroundStyle = {
     "--step": "5%",
@@ -50,69 +50,69 @@ export const GlareCard = ({
     "--shade":
       "radial-gradient( farthest-corner circle at var(--m-x) var(--m-y),rgba(255,255,255,0.1) 12%,rgba(255,255,255,0.15) 20%,rgba(255,255,255,0.25) 120% ) var(--bg-x) var(--bg-y)/300% no-repeat",
     backgroundBlendMode: "hue, hue, hue, overlay",
-  };
+  }
 
   const updateStyles = () => {
     if (refElement.current) {
-      console.log(state.current);
-      const { background, rotate, glare } = state.current;
-      refElement.current?.style.setProperty("--m-x", `${glare.x}%`);
-      refElement.current?.style.setProperty("--m-y", `${glare.y}%`);
-      refElement.current?.style.setProperty("--r-x", `${rotate.x}deg`);
-      refElement.current?.style.setProperty("--r-y", `${rotate.y}deg`);
-      refElement.current?.style.setProperty("--bg-x", `${background.x}%`);
-      refElement.current?.style.setProperty("--bg-y", `${background.y}%`);
+      console.log(state.current)
+      const { background, rotate, glare } = state.current
+      refElement.current?.style.setProperty("--m-x", `${glare.x}%`)
+      refElement.current?.style.setProperty("--m-y", `${glare.y}%`)
+      refElement.current?.style.setProperty("--r-x", `${rotate.x}deg`)
+      refElement.current?.style.setProperty("--r-y", `${rotate.y}deg`)
+      refElement.current?.style.setProperty("--bg-x", `${background.x}%`)
+      refElement.current?.style.setProperty("--bg-y", `${background.y}%`)
     }
-  };
+  }
   return (
     <div
       style={containerStyle}
       className="relative isolate [contain:layout_style] [perspective:600px] transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-transform w-[320px] [aspect-ratio:17/21]"
       ref={refElement}
       onPointerMove={(event) => {
-        const rotateFactor = 0.4;
-        const rect = event.currentTarget.getBoundingClientRect();
+        const rotateFactor = 0.4
+        const rect = event.currentTarget.getBoundingClientRect()
         const position = {
           x: event.clientX - rect.left,
           y: event.clientY - rect.top,
-        };
+        }
         const percentage = {
           x: (100 / rect.width) * position.x,
           y: (100 / rect.height) * position.y,
-        };
+        }
         const delta = {
           x: percentage.x - 50,
           y: percentage.y - 50,
-        };
+        }
 
-        const { background, rotate, glare } = state.current;
-        background.x = 50 + percentage.x / 4 - 12.5;
-        background.y = 50 + percentage.y / 3 - 16.67;
-        rotate.x = -(delta.x / 3.5);
-        rotate.y = delta.y / 2;
-        rotate.x *= rotateFactor;
-        rotate.y *= rotateFactor;
-        glare.x = percentage.x;
-        glare.y = percentage.y;
+        const { background, rotate, glare } = state.current
+        background.x = 50 + percentage.x / 4 - 12.5
+        background.y = 50 + percentage.y / 3 - 16.67
+        rotate.x = -(delta.x / 3.5)
+        rotate.y = delta.y / 2
+        rotate.x *= rotateFactor
+        rotate.y *= rotateFactor
+        glare.x = percentage.x
+        glare.y = percentage.y
 
-        updateStyles();
+        updateStyles()
       }}
       onPointerEnter={() => {
-        isPointerInside.current = true;
+        isPointerInside.current = true
         if (refElement.current) {
           setTimeout(() => {
             if (isPointerInside.current) {
-              refElement.current?.style.setProperty("--duration", "0s");
+              refElement.current?.style.setProperty("--duration", "0s")
             }
-          }, 300);
+          }, 300)
         }
       }}
       onPointerLeave={() => {
-        isPointerInside.current = false;
+        isPointerInside.current = false
         if (refElement.current) {
-          refElement.current.style.removeProperty("--duration");
-          refElement.current?.style.setProperty("--r-x", `0deg`);
-          refElement.current?.style.setProperty("--r-y", `0deg`);
+          refElement.current.style.removeProperty("--duration")
+          refElement.current?.style.setProperty("--r-x", "0deg")
+          refElement.current?.style.setProperty("--r-y", "0deg")
         }
       }}
     >
@@ -129,5 +129,5 @@ export const GlareCard = ({
         />
       </div>
     </div>
-  );
-};
+  )
+}
