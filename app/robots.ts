ab617b3dad8bb0bfbbc6eda603bd3a25f1c22178
Nazim Gafarov <zapolnoch@gmail.com>
2024-07-28 06:05:01 +0300

    robots: default function

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import { HOSTNAME } from "@/app/config"
+import type { MetadataRoute } from "next"
+import { headers } from "next/headers"
+
+export default function robots(): MetadataRoute.Robots {
+  const host = headers().get("host")
+  const isProd = host === HOSTNAME
+
+  return {
+    rules: {
+      userAgent: "*",
+      disallow: isProd ? "/api" : "/",
+    },
+  }
+}
