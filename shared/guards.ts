ab6f08234057fe9eab17caab3c6808e1740f1437
Nazim Gafarov <zapolnoch@gmail.com>
2024-08-12 17:41:57 +0300

    auth: redirect

--- shared/guards.ts
+++ shared/guards.ts
@@ -2,33 +2,29 @@ import { cookies, headers } from "next/headers"
 import { redirect } from "next/navigation"
 
 export async function isLoggedIn() {
+  let isLogged = false
+
   try {
     const cookie = cookies().get("frkn_auth")
     const host = headers().get("host")
     const protocol = headers().get("x-forwarded-proto") || "http"
     const currentUrl = `${protocol}://${host}`
-    console.log("host", host, protocol, currentUrl)
-    console.log("cookie", cookie)
 
-    if (!cookie) {
-      redirect("/registration")
-      return
+    if (cookie) {
+      const data = await fetch(currentUrl + "/api/user/me", {
+        headers: {
+          Cookie: cookie.name + "=" + cookie.value,
+        },
+      })
+      const auth = await data.json()
+
+      if (auth.status === "success") {
+        isLogged = true
+      }
     }
-
-    const data = await fetch(currentUrl + "/api/user/me", {
-      headers: {
-        Cookie: cookie.name + "=" + cookie.value,
-      },
-    })
-    const auth = await data.json()
-
-    if (!auth || auth.status === "error") {
-      redirect("/registration")
-      return
-    }
-
-    return auth
   } catch (error) {
     console.error("isLoggedIn", error)
+  } finally {
+    if (!isLogged) redirect("/registration")
   }
 }
