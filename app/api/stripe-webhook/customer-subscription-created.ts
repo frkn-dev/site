8b9eaa473b7f93afcaf64ae28b7bc287cde2507b
Nazim Gafarov <zapolnoch@gmail.com>
2024-09-18 19:51:47 +0300

    stripe: events

--- app/api/stripe-webhook/customer-subscription-created.ts
+++ app/api/stripe-webhook/customer-subscription-created.ts
@@ -1,6 +1,7 @@
 import type { Stripe } from "stripe"
 
 import prisma from "@/prisma"
+import { upgrade } from "@/shared/trpc/routers"
 
 export async function customerSubscriptionCreated(
   event: Stripe.CustomerSubscriptionCreatedEvent,
@@ -10,7 +11,7 @@ export async function customerSubscriptionCreated(
     const userId = subscription.metadata.userId
 
     if (!userId) {
-      throw new Error("User id not found in subscription metadata")
+      throw new Error("User id not found in subscription metadata: " + userId)
     }
 
     const promises = [
@@ -29,6 +30,12 @@ export async function customerSubscriptionCreated(
     ]
 
     await Promise.all(promises)
+
+    if (subscription.status === "active") {
+      await upgrade(userId)
+    } else {
+      console.error("Stripe: unexpected status", subscription.status)
+    }
 
     await prisma.stripeSubscriptionItems.createMany({
       data: subscription.items.data.map((item) => ({
--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -1,13 +1,15 @@
 import { env } from "@/env"
 import { getErrorMessage } from "@/shared/get-error-message"
 import { stripe } from "@/shared/services/stripe/client"
+import { headers } from "next/headers"
+import { customerSubscriptionCreated } from "./customer-subscription-created"
 import { customerSubscriptionDeleted } from "./customer-subscription-deleted"
 import { customerSubscriptionUpdated } from "./customer-subscription-updated"
 
 export async function POST(request: Request) {
   try {
     const body = await request.text()
-    const signature = request.headers.get("stripe-signature")
+    const signature = headers().get("stripe-signature")
 
     if (!signature) {
       throw new Error("Missing stripe signature")
@@ -20,6 +22,10 @@ export async function POST(request: Request) {
     )
 
     switch (event.type) {
+      case "customer.subscription.created": {
+        await customerSubscriptionCreated(event)
+        break
+      }
       case "customer.subscription.updated": {
         await customerSubscriptionUpdated(event)
         break
